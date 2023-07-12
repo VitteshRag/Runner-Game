1eab6dba31856807b2fa8b32de2ddd0985452a82
Vittesh Raghavan <vitteshraghavan@gmail.com>
2023-07-12 22:27:29 +0530

    Added Main Menu & Restart Button UI

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void onQuit()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
